0412f75636e83e79c18eb991973b95f5c51ed3dc
guilherme de araujo <guiam2909@gmail.com>
2026-08-20 08:59:58 -0300

    feat: erro gerenerico, conversao, aritmetico, diferentes erros em diferentes areas do codigo

--- src/br/com/fiap/main/Exemplo.java
+++ src/br/com/fiap/main/Exemplo.java
@@ -1,11 +1,29 @@
 package br.com.fiap.main;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Exemplo {
     static void main() {
-        try {
-            int numero = 10 / 0;
+        Scanner scan = new Scanner(System.in);
+        try  {
+            System.out.println("Digite um numero inteiro: ");
+            int num0 = scan.nextInt();
+            if (num0 < 1 || num0 > 10) {
+                throw new Exception("Valor informado fora da faixa permitida");
+            }
+            System.out.println("Digite um segundo numero inteiro: ");
+            int num1 = scan.nextInt();
+            int numero = num0 / num1;
+        } catch (ArithmeticException e) {
+            System.out.println("Erro aritmetico: " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Erro de conversao: " + e.getMessage());
         } catch (Exception e) {
             System.out.println("Erro generico: " + e.getMessage());
+        } finally {
+            scan.close();
+            System.out.println("fim de programa"); // comandos que sempre querem ser executados, associado a coisas mais delicadas, por exemplo em banco de dados
         }
     }
 }
